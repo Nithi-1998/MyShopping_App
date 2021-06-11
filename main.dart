152231637c2ad8618392b16ee733af05bfe06362
JOY <supavadee.ma@ku.th>
2021-06-11 22:02:48 +0700

    make Shopping App

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Scaffold(
+        body : MyshopApp(),
+      ),
+    );
+  }
+}
+class MyshopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
+
